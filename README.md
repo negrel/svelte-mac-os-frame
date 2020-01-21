0a3b30b91289fab9db655a72fb0f48927d8cb6e9
nergel3 <alexnergel13@gmail.com>
2020-01-21 20:24:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+![package size](https://img.shields.io/github/size/nergel3/svelte-mac-os-frame/src/index.svelte)
+![npm version](https://img.shields.io/npm/v/svelte-mac-os-frame-div?color=green)
+# svelte-mac-os-frame
+
+> A mac os frame like svelte component.
+
+### Installation
+
+```
+npm install svelte-mac-os-frame
+```
+
+### Getting started
+
+```svelte
+<script>
+	// Example of svelte-mac-os-frame component.
+	// After 'npm install svelte-mac-os-frame' you can use it :
+	import MacOsFrame from 'svelte-mac-os-frame';
+
+	const title = 'svelte_logo.jpg';
+</script>
+
+<MacOsFrame {title}>
+	<img src="https://svelte.dev/svelte-logo-horizontal.svg" alt={title}>
+</MacOsFrame>
+```
+
+### :stars: Show Your Support
+Please give a :star: if this project helped you!
+
+#### :scroll: License
+MIT © Alexandre Negrel
+
---
+++ src/index.svelte
@@ -0,0 +1,73 @@
+<div class="mac-os-frame">
+	<div class="mac-os-frame_header">
+		<div class="mac-os-frame_header_buttons">
+			<div class="mac-os-frame_header_button_left" />
+			<div class="mac-os-frame_header_button_middle" />
+			<div class="mac-os-frame_header_button_right" />
+		</div>
+		<div class="mac-os-frame_header_title">
+			{ title }
+		</div>
+	</div>
+	<div class="mac-os-frame_body">
+		<slot />
+	</div>
+</div>
+
+<script>
+// Title props
+export let title = "";
+</script>
+
+<style>
+.mac-os-frame {
+	border-radius: .5em;
+	overflow: hidden;
+	box-shadow: 0 7px 8px -4px rgba(0, 0, 0, 0.2), 0 12px 17px 2px rgba(0, 0, 0, 0.14), 0 5px 22px 4px rgba(0, 0, 0, 0.12);
+}
+
+.mac-os-frame_header {
+	position: relative;
+	padding: .5em .75em;
+	display: flex;
+	justify-content: space-between;
+	background: rgb(201,200,206);
+	background: linear-gradient(0deg, rgba(201,200,206,1) 0%, rgba(230,229,241,1) 100%);}
+
+.mac-os-frame_header * {
+	display: inline-block
+}
+
+.mac-os-frame_header_title {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	opacity: .5;
+	font-size: .75em;
+	color: black;
+}
+
+.mac-os-frame_header_buttons > * {
+	width: .75em;
+	height: .75em;
+	margin: 0 .25em;
+	border-radius: 100%;
+}
+
+.mac-os-frame_header_buttons > .mac-os-frame_header_button_left {
+	background-color: red;
+}
+
+.mac-os-frame_header_buttons > .mac-os-frame_header_button_middle {
+	background-color: orange;
+}
+
+.mac-os-frame_header_buttons > .mac-os-frame_header_button_right {
+	background-color: #78bb65;
+}
+
+.mac-os-frame_body {
+	background-color: white;
+}
+</style>
